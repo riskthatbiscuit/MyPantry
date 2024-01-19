e7047e4f077f0d9fa9c364c5b1bd8eec51af77a8
Casey Mcgrath <caseymcgrath1000@gmail.com>
2024-01-19 09:27:25 -0500

    Initial commit

---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const notes = express.Router();
+const {readFromFile, readAndAppend} = require('../helpers/fsutils.js');
+
+
+notes.get('/', (req, res) => {
+    console.log(`A ${req.method} request was made`);
+    readFromFile('./db/notes.json').then((data => res.json(JSON.parse(data))));
+})
+
+notes.post('/', (req, res) => {
+    console.log(`A ${req.method} request was made`);
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text
+        };
+
+        readAndAppend(newNote, './db/notes.json');
+        res.json('Note added successfully')
+    } else {
+        res.error('Error adding Note')
+    }
+})
+
+module.exports = notes;
